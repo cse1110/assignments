8d1dacdfd3beec8224dd6cfafde5a14e9bf13171
Mauricio Aniche <mauricioaniche@gmail.com>
2021-09-18 20:08:42 +0200

    name of the solution class

--- solutions/domain-testing/add/NumberUtilsTest.java
+++ solutions/domain-testing/add/NumberUtilsTest.java
@@ -10,7 +10,7 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
-class NumberUtilsTests {
+class NumberUtilsTest {
 
 	@ParameterizedTest
 	@MethodSource("testCases")
