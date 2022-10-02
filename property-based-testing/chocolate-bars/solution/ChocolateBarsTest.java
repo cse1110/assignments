46db770c556f5e0ea4508c18943c9c5b109e10a6
Martin Mladenov <30376060+martinmladenov@users.noreply.github.com>
2022-10-02 23:02:45 +0200

    Fix mistake in assignment

--- property-based-testing/chocolate-bars/solution/ChocolateBarsTest.java
+++ property-based-testing/chocolate-bars/solution/ChocolateBarsTest.java
@@ -17,27 +17,27 @@ class ChocolateBarsTest {
 	private final ChocolateBars chocolateBars = new ChocolateBars();
 
 	@Property
-	void onlySmallBars(@ForAll("onlySmall") ChocolateBarsTestInput input) {
+	void onlySmallBars(@ForAll("onlySmall") ChocolateBarsInput input) {
 		assertThat(chocolateBars.calculate(input.small, input.big, input.total)).isEqualTo(input.total);
 	}
 
 	@Property
-	void onlyBigBars(@ForAll("onlyBig") ChocolateBarsTestInput input) {
+	void onlyBigBars(@ForAll("onlyBig") ChocolateBarsInput input) {
 		assertThat(chocolateBars.calculate(input.small, input.big, input.total)).isEqualTo(0);
 	}
 
 	@Property
-	void bothBars(@ForAll("both") ChocolateBarsTestInput input) {
+	void bothBars(@ForAll("both") ChocolateBarsInput input) {
 		assertThat(chocolateBars.calculate(input.small, input.big, input.total))
 				.isEqualTo(input.total - Math.min(input.total / 5, input.big) * 5);
 	}
 
 	@Property
-	void noBars(@ForAll("none") ChocolateBarsTestInput input) {
+	void noBars(@ForAll("none") ChocolateBarsInput input) {
 		assertThat(chocolateBars.calculate(input.small, input.big, input.total)).isEqualTo(-1);
 	}
 
-	static class ChocolateBarsTestInput {
+	static class ChocolateBarsInput {
 
 		int small;
 
@@ -45,7 +45,7 @@ class ChocolateBarsTest {
 
 		int total;
 
-		public ChocolateBarsTestInput(int small, int big, int total) {
+		public ChocolateBarsInput(int small, int big, int total) {
 			this.small = small;
 			this.big = big;
 			this.total = total;
@@ -53,38 +53,38 @@ class ChocolateBarsTest {
 	}
 
 	@Provide
-	private Arbitrary<ChocolateBarsTestInput> onlySmall() {
+	private Arbitrary<ChocolateBarsInput> onlySmall() {
 		IntegerArbitrary small = Arbitraries.integers().greaterOrEqual(0);
 		IntegerArbitrary big = Arbitraries.integers().greaterOrEqual(0);
 		IntegerArbitrary total = Arbitraries.integers().greaterOrEqual(0);
-		return Combinators.combine(small, big, total).as(ChocolateBarsTestInput::new)
+		return Combinators.combine(small, big, total).as(ChocolateBarsInput::new)
 				.filter(k -> (k.total < 5 || k.big == 0) && k.small >= k.total);
 	}
 
 	@Provide
-	private Arbitrary<ChocolateBarsTestInput> onlyBig() {
+	private Arbitrary<ChocolateBarsInput> onlyBig() {
 		IntegerArbitrary small = Arbitraries.integers().greaterOrEqual(0);
 		IntegerArbitrary big = Arbitraries.integers().greaterOrEqual(0);
 		IntegerArbitrary total = Arbitraries.integers().greaterOrEqual(0);
-		return Combinators.combine(small, big, total).as(ChocolateBarsTestInput::new)
+		return Combinators.combine(small, big, total).as(ChocolateBarsInput::new)
 				.filter(k -> k.total <= 5 * k.big && k.total % 5 == 0);
 	}
 
 	@Provide
-	private Arbitrary<ChocolateBarsTestInput> both() {
+	private Arbitrary<ChocolateBarsInput> both() {
 		IntegerArbitrary small = Arbitraries.integers().greaterOrEqual(1);
 		IntegerArbitrary big = Arbitraries.integers().greaterOrEqual(1);
 		IntegerArbitrary total = Arbitraries.integers().greaterOrEqual(1);
-		return Combinators.combine(small, big, total).as(ChocolateBarsTestInput::new)
+		return Combinators.combine(small, big, total).as(ChocolateBarsInput::new)
 				.filter(k -> k.total - k.big * 5 > 0 && k.small >= k.total - k.big * 5);
 	}
 
 	@Provide
-	private Arbitrary<ChocolateBarsTestInput> none() {
+	private Arbitrary<ChocolateBarsInput> none() {
 		IntegerArbitrary small = Arbitraries.integers().greaterOrEqual(0);
 		IntegerArbitrary big = Arbitraries.integers().greaterOrEqual(0);
 		IntegerArbitrary total = Arbitraries.integers().greaterOrEqual(1);
-		return Combinators.combine(small, big, total).as(ChocolateBarsTestInput::new)
+		return Combinators.combine(small, big, total).as(ChocolateBarsInput::new)
 				.filter(k -> k.small < k.total - Math.min(k.total / 5, k.big) * 5);
 	}
 }
